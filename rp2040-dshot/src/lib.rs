ef3482a16e347fcd5a8cfd9ecad73bbab7ad7f8f
Rtificer <rtificr@gmail.com>
2025-12-20 00:28:42 -0500

    fixed feature errors

--- rp2040-dshot/src/lib.rs
+++ rp2040-dshot/src/lib.rs
@@ -26,23 +26,30 @@ pub enum Error {
     #[error("Throttle command must be in the range 0-1999, was {throttle}")]
     ThrottleBoundsError { throttle: u16 },
     /// Clock divider conversion from float to fixed failed in clock divisor calculation.
+    #[cfg(feature = "driver")]
     #[error("Conversion from float to fixed failed in clock divider calculation. Float value: {}", clock_divider_float)]
     ClockDividerConversionError { clock_divider_float: f32 },
     /// Failed to spawn task.
+    #[cfg(feature = "driver")]
     #[error("Failed to spawn task")]
     SpawnError(#[from] embassy_executor::SpawnError),
     /// Failed to recieve value from channel.
+    #[cfg(feature = "driver")]
     #[error("Failed to recieve value from channel")]
     TryReceiveError(#[from] embassy_sync::channel::TryReceiveError),
     /// Driver future timed out
+    #[cfg(feature = "driver")]
     #[error("Driver future timed out")]
     TimeoutError(#[from] embassy_time::TimeoutError),
     /// Invalid ERPM telemerty checksum.
+    #[cfg(feature = "driver")]
     #[error("Invalid ERPM telemetry checksum")]
     InvalidTelemetryChecksum,
     /// TX Push Faliure
+    #[cfg(feature = "driver")]
     #[error("TX Push Faliure")]
     TxTryPushFaliure,
+    #[cfg(feature = "driver")]
     /// State machine split faliure, empty pointer!
     #[error("SM Split Faliure, empty pointer!")]
     SmSplitFaliure,
